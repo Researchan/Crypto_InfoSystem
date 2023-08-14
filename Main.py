991aaf217a9a9ce21c997ed89d4b8a1bbd11a943
researchan <kimcw97@gmail.com>
2023-08-15 01:12:49 +0900

    Update data

---
+++ Main.py
@@ -0,0 +1,9 @@
+import os
+import time
+
+while True:
+    os.system('ExecutionScript.py')
+    os.system('git add .')
+    os.system('git commit -m "Update data"')
+    os.system('git push')
+    time.sleep(300)
