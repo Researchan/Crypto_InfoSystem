c7b96ddcb79adf0ca337f5a157013b97eb89f590
researchan <kimcw97@gmail.com>
2023-09-12 00:32:38 +0900

    뭔가하려고하는중

---
+++ Exchange_listing_info/Tools/Get_Bithumb_list copy.py
@@ -0,0 +1,17 @@
+import ccxt
+
+exBithumb = ccxt.bithumb({})
+exBithumbTickersInfo = exBithumb.fetchTickers() # 티커 딕셔너리 가져옴
+exBithumbTickers = exBithumbTickersInfo.keys() # 티커 키만 받아오기 (이름만)
+
+BTClist = []
+KRWlist = []
+Alllist = []
+for i in exBithumbTickers:
+    if i[-3:] == 'BTC':
+        BTClist.append(i)
+    elif i[-3:] == 'KRW':
+        KRWlist.append(i)
+
+BTClist.remove()
+KRWlist.remove()
