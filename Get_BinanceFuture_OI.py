1debcefec5d9de015050cba231bb5a5ebd90f2ee
researchan <kimcw97@gmail.com>
2024-05-14 10:34:01 +0900

    Update data

--- Get_BinanceFuture_OI.py
+++ Get_BinanceFuture_OI.py
@@ -47,7 +47,7 @@ for key, value in OI_Dict.items():
     New_OI_Dict[new_key] = value
     
 # #페어 이름정보 변경 (수동)
-New_OI_Dict['DODO'] = New_OI_Dict.pop('DODOX')
+# New_OI_Dict['DODO'] = New_OI_Dict.pop('DODOX')
 New_OI_Dict['FLOKI'] = New_OI_Dict.pop('1000FLOKI')
 New_OI_Dict['LUNC'] = New_OI_Dict.pop('1000LUNC')
 New_OI_Dict['PEPE'] = New_OI_Dict.pop('1000PEPE')
@@ -56,7 +56,7 @@ New_OI_Dict['XEC'] = New_OI_Dict.pop('1000XEC')
 New_OI_Dict['SATS'] = New_OI_Dict.pop('1000SATS')
 New_OI_Dict['RATS'] = New_OI_Dict.pop('1000RATS')
 New_OI_Dict['BONK'] = New_OI_Dict.pop('1000BONK')
-New_OI_Dict['BEAM'] = New_OI_Dict.pop('BEAMX') #C98 리브랜딩
+# New_OI_Dict['BEAM'] = New_OI_Dict.pop('BEAMX') #C98 리브랜딩
 # New_OI_Dict['VIC'] = New_OI_Dict.pop('TOMO') #토모 리브랜딩
 
 #OI 내림차순
