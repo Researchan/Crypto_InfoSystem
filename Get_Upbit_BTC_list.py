af3ddc1b0ba71fe238011959417e0b1c970f8486
researchan <kimcw97@gmail.com>
2024-05-22 16:55:27 +0900

    Update data

--- Get_Upbit_BTC_list.py
+++ Get_Upbit_BTC_list.py
@@ -1,23 +1,28 @@
 import ccxt
+import jandimodule
 
-exUpbit = ccxt.upbit({})
-exUpbitTickersInfo = exUpbit.fetchTickers() # 티커 딕셔너리 가져옴
-exUpbitTickers = exUpbitTickersInfo.keys() # 티커 키만 받아오기 (이름만)
+try:
+    exUpbit = ccxt.upbit({})
+    exUpbitTickersInfo = exUpbit.fetchTickers() # 티커 딕셔너리 가져옴
+    exUpbitTickers = exUpbitTickersInfo.keys() # 티커 키만 받아오기 (이름만)
 
-Tickerlist = []
-for i in exUpbitTickers:
-    if i[-3:] == 'BTC':
-        Tickerlist.append(i[0:-4])
+    Tickerlist = []
+    for i in exUpbitTickers:
+        if i[-3:] == 'BTC':
+            Tickerlist.append(i[0:-4])
 
-# Tokamak이 TON이랑 겹침. 또한 오름차순 정렬시 소문자 O를 엑셀이 구분하기때문에 전부 대문자로 변경
-Tickerlist.remove('Tokamak Network')
-Tickerlist.append('TOKAMAK')
+    # Tokamak이 TON이랑 겹침. 또한 오름차순 정렬시 소문자 O를 엑셀이 구분하기때문에 전부 대문자로 변경
+    Tickerlist.remove('Tokamak Network')
+    Tickerlist.append('TOKAMAK')
 
-Tickerlist.remove('GAME2')
-# Tickerlist.remove('PDA')
-Tickerlist = set(Tickerlist)
-Tickerlist = list(Tickerlist)
-Tickerlist.sort()
+    # Tickerlist.remove('GAME2')
+    # Tickerlist.remove('PDA')
+    Tickerlist = set(Tickerlist)
+    Tickerlist = list(Tickerlist)
+    Tickerlist.sort()
 
-# for i in Tickerlist:
-#     print(i)
+    # for i in Tickerlist:
+    #     print(i)
+    
+except Exception as e:
+    jandimodule.Exchange_Listing_send_message_to_jandi(str(e))
