5a2122341d447c7051d1e4a061e65a242ec3576b
Researchan <109773577+Researchan@users.noreply.github.com>
2024-10-17 22:27:04 +0900

    Update data

--- Get_BinanceSpot_list.py
+++ Get_BinanceSpot_list.py
@@ -53,6 +53,7 @@ Tickerlist.remove('XEM')
 Tickerlist.remove('AGIX') # FET합병
 Tickerlist.remove('OCEAN') # FET 합병
 Tickerlist.remove('SCR') # 임시 보류
+Tickerlist.remove('BNSOL') # 임시 보류
 
 #LUNA는 LUNA2로 티커이름 통일하겠음.
 Tickerlist.remove('LUNA')
