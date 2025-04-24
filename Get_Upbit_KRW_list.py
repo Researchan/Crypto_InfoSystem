8dfd1489a5f1c6e58837fa47f23c054a10af3bd8
Researchan <109773577+Researchan@users.noreply.github.com>
2025-04-24 14:49:37 +0900

    Revert "Update Get_Upbit_KRW_list.py"
    This reverts commit 6a9a5c46c2db9ef8107692d017ffac81f27f961d.

--- Get_Upbit_KRW_list.py
+++ Get_Upbit_KRW_list.py
@@ -1,27 +1,32 @@
 import ccxt
+import jandimodule
 
-exUpbit = ccxt.upbit({})
-exUpbitTickersInfo = exUpbit.fetchTickers() # 티커 딕셔너리 가져옴
-exUpbitTickers = exUpbitTickersInfo.keys() # 티커 키만 받아오기 (이름만)
+try:
+    exUpbit = ccxt.upbit({})
+    # 모든 심볼 가져오기
+    all_symbols = exUpbit.load_markets().keys()
+    # KRW 페어만 필터링
+    krw_symbols = [symbol for symbol in all_symbols if '/KRW' in symbol]
 
-Tickerlist = []
-for i in exUpbitTickers:
-    if '/KRW' in i:
-        Tickerlist.append(i[0:-4])
+    # 심볼 목록을 여러 개의 작은 그룹으로 나누기
+    chunk_size = 100
+    symbol_chunks = [krw_symbols[i:i + chunk_size] for i in range(0, len(krw_symbols), chunk_size)]
 
-# Tokamak이 TON이랑 겹침. 또한 오름차순 정렬시 소문자 O를 엑셀이 구분하기때문에 전부 대문자로 변경
-#Tickerlist.remove('Tokamak Network')
-#Tickerlist.append('TOKAMAK')
+    Tickerlist = []
+    for chunk in symbol_chunks:
+        # 각 그룹에 대해 fetchTickers() 메서드 호출
+        tickers_info = exUpbit.fetch_tickers(chunk)
+        for i in tickers_info.keys():
+            Tickerlist.append(i[0:-4])
 
-Tickerlist.remove('GAME2') #CoinGecko에 정보없음.
-Tickerlist.remove('USDT') #테더삭제
-# Tickerlist.remove('BOUNTY') #코개코랑 코마캡에 없음 생김.
+    # Tokamak이 TON이랑 겹침. 또한 오름차순 정렬시 소문자 O를 엑셀이 구분하기때문에 전부 대문자로 변경
+    #Tickerlist.remove('Tokamak Network')
+    #Tickerlist.append('TOKAMAK')
 
-Tickerlist = set(Tickerlist)
-Tickerlist = list(Tickerlist)
-Tickerlist.sort()
+    Tickerlist.remove('GAME2') #CoinGecko에 정보없음.
+    Tickerlist.remove('USDT') #테더삭제
+    # Tickerlist.remove('BOUNTY') #코개코랑 코마캡에 없음 생김.
 
-<<<<<<< HEAD
     Tickerlist = set(Tickerlist)
     Tickerlist = list(Tickerlist)
     Tickerlist.sort()
@@ -34,7 +39,3 @@ except Exception as e:
     error_message = f"업비트 데이터 수집 중 오류 발생: {str(e)}"
     print(error_message)
     jandimodule.Exchange_Listing_send_message_to_jandi(error_message)
-=======
-# for i in Tickerlist:
-#     print(i)
->>>>>>> parent of a446c7b3b (Update data)
