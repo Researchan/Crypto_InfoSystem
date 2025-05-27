9239b0ea4ac885af3473ba1b0ae49fc1b9b2d45b
Researchan <109773577+Researchan@users.noreply.github.com>
2025-05-27 15:42:25 +0900

    Update data

--- Get_Bithumb_list.py
+++ Get_Bithumb_list.py
@@ -34,6 +34,7 @@ Tickerlist.remove('USDT')
 # Tickerlist.remove('XENT') # ENTC 리브랜딩. 안나옴 제외. /결국 상폐당함
 Tickerlist.remove('FLZ') #빗썸 단독상장 개잡코인데 FDV 3B 이라서 짜증나서 제외함. 어차피 재단만 갖고있는 쓰레기
 # Tickerlist.remove('HPO') 
+# Tickerlist.remove('HP') #업비트랑 둘만 상장된 김치. 정보도 잘 없어서 제외
 
 Tickerlist = set(Tickerlist)
 Tickerlist = list(Tickerlist)
